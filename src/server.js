cb9325caaa637b3be912d04d32322780a64a5614
Phong <phongit1995@gmail.com>
2019-07-28 06:28:33 +0700

    lan 2

--- src/server.js
+++ src/server.js
@@ -4,4 +4,6 @@ let app = express();
 app.get("/",(req,res)=>{
     res.send("Phong Nguyễn");
 })
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT,()=>{
+    console.log("Đang Chạy Trên Port :" + process.env.APP_PORT);
+});
