99a93628b372c72c1c1c59208dc2142467e41217
Phong <phongit1995@gmail.com>
2019-07-28 17:15:43 +0700

    fix connect to mông

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
 import express from "express";
+import ContactModel from "./models/contact.model";
+import DBConnect from "./config/connectdb";
 require('dotenv').config()
 let app = express();
+DBConnect();
 app.get("/",(req,res)=>{
     res.send("Phong Nguyễn");
+})
+app.get("/test", async(req,res)=>{
+    try{
+        let item ={
+            userID:"phong",
+            contactID:"Nguyen"
+        }
+        let contact = await ContactModel.createNew(item);
+        console.log(contact);
+        res.send("Phong" + contact);
+
+    }
+    catch(erro){
+        Console.log(erro);
+    }
 })
 app.listen(process.env.APP_PORT,()=>{
     console.log("Đang Chạy Trên Port :" + process.env.APP_PORT);
